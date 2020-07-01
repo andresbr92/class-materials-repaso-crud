5a7078ecbd11f29386e686ed1d6db911b588c67e
andresbr92 <andresbr92@gmail.com>
2020-07-01 18:58:31 +0200

    done

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -22,27 +22,48 @@ router.get('/new', (req, res) => {
 
 router.get('/', (req, res) => {
     Coaster
-        .find() 
-        .populate ('park')
+        .find()
+        .populate('park')
         .then(allCoasters => {
-            res.render('coasters/coasters-index', {allCoasters})   
-         })
+            res.render('coasters/coasters-index', { allCoasters })
+        })
+        .catch(err => console.log("Error en la BBDD", err))
 })
 
-router.get('/:id', (req, res) => { 
+router.get('/:id', (req, res) => {
     Coaster
         .findById(req.params.id)
-        .populate ('park')
-        .then(theCoaster => { 
-            res.render('coasters/coaster-details', { theCoaster } )
-            
-            
+        .populate('park')
+        .then(theCoaster => {
+            res.render('coasters/coaster-details', { theCoaster })
+
+
         })
+        .catch(err => console.log("Error en la BBDD", err))
 })
-router.get('/delete/:id', (req, res) => { 
+router.get('/delete/:id', (req, res) => {
     Coaster
         .findByIdAndRemove(req.params.id)
-    .then (res.redirect ('/coasters'))
+        .then(res.redirect('/coasters'))
+        .catch(err => console.log("Error en la BBDD", err))
+})
+
+router.get('/edit/:id', (req, res) => {
+
+    Promise.all([Park.find(), Coaster.findById(req.params.id)])
+        .then(values => {
+            console.log (values[0])
+            res.render('coasters/coaster-edit', {thePark:values[0], theCoaster: values[1]})
+        })
+    
+    // Coaster.findById(req.params.id)
+        
+    //     .then(theCoaster => {
+    //         res.render('coasters/coaster-edit', { theCoaster })
+    //     })
+    //     .catch(err => console.log("Error en la BBDD", err))
+   
+    
 })
 
 //RUTAS POST <==============    
@@ -52,6 +73,16 @@ router.post('/new', (req, res) => {
     Coaster
         .create({ name, description, inversions, length, park })
         .then(res.redirect('/coasters/new'))
+        .catch(err => console.log("Error en la BBDD", err))
+})
+
+
+router.post('/edit/:id', (req, res) => {
+    const { name, description, inversions, length, park } = req.body
+    Coaster
+        .findByIdAndUpdate(req.params.id, { name, description, inversions, length, park })
+        .then(res.redirect('/coasters'))
+        .catch(err => console.log("Error en la BBDD", err))
 })
 
 
